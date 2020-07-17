8dd2970b583ea047c6c4939d0b53f11a1a7a43aa
robinrbh <rhartog@gmail.com>
2020-07-17 09:32:50 +0200

    Pokemon stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
 	return (
 		<div className="App">
+			<main>
+				<Title content="Some simple title" />
+				<Pokemon name="Charizard" weight="90" awesome="true" terrifying="false" abilities="Blaze, Solar power, Tough claws, Drought"/>
+				<Pokemon name="Bulbasaur" weight="6.9" awesome="true" terrifying="false" abilities="Overgrow, Chlorophyll"/>
+				<Pokemon name="Mewtwo" weight="122" awesome="true" terrifying="true" abilities="Pressure, Unnerve, Steadfast, Insomnia"/>
+				<Pokemon name="Mega beedrill" weight="65" awesome="false" terrifying="true" abilities="Intimidate, Unnerve"/>
+			</main>
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
